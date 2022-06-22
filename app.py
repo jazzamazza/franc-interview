5ad10701b204cb0c1ed6194f28139525c37511f2
Jared May <jared@jaredmay.co.za>
2022-06-22 22:49:18 +0200

    modified:   app.py deleted:    app.pyc modified:   static/css/main.css modified:   templates/base.html modified:   templates/index.html modified:   templates/timeline.html deleted:    test.db

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
+from time import time
 from flask import Flask, render_template, jsonify, Response, request, url_for
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Timeline(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    post = db.Column(db.String(256), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Post %r>' % self.id
 
 @app.route('/') #could add methods post and get
 def index_view():
@@ -36,9 +27,46 @@ def posts_view():
         posts = f.read()
     return Response(posts, mimetype="application/json")
 
-@app.route('/test')
+@app.route('/test/', methods=['POST', 'GET'])
 def test_view():
-    return "Hello World!"
+    username = request.args.get('username')
+    #print(username)
+    if username==None:
+        username = 'Franc'
+    
+    #load users.json
+    with open('./users.json', 'r') as f:
+        users = f.read()
+    users_data = json.loads(users)
+        
+    user_following = users_data[username]
+    
+    with open('./posts.json', 'r') as f:
+        posts = f.read()
+    posts_data = json.loads(posts)
+    user_posts = posts_data[username]
+    
+    #timeline = 
+    
+    for user in user_following:
+        with open('./posts.json', 'r') as f:
+            posts = f.read()
+            
+    user_following.append(username)
+    
+    
+    
+    #filter
+    for post in user_posts:
+        for key in post:
+            print(post[key])
+            
+    #dict1 = user_posts[0]
+    #print(dict1['status'])
+    return '''
+            The posts for {} are: {}
+            they are following: {} '''.format(username, user_posts, user_following)
+    #return Response(data, mimetype="application/json")
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
