b07fff49d4333c095f2173733e0d4a037bd6a666
Jared May <myxjar002@myuct.ac.za>
2022-06-22 17:25:05 +0200

    modified:   app.py new file:   app.pyc new file:   static/css/main.css new file:   templates/base.html modified:   templates/index.html new file:   templates/timeline.html new file:   test.db

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, jsonify, Response, request
+from flask import Flask, render_template, jsonify, Response, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Timeline(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+@app.route('/') #could add methods post and get
 def index_view():
     username = request.args.get('username')
+    #this loads the webpage from template file
     return render_template('index.html', username = username)
+
+@app.route('/timeline')
+def timeline_view():
+    return render_template('timeline.html')
 
 @app.route('/users')
 def users_view():
@@ -19,5 +36,9 @@ def posts_view():
         posts = f.read()
     return Response(posts, mimetype="application/json")
 
+@app.route('/test')
+def test_view():
+    return "Hello World!"
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1')
